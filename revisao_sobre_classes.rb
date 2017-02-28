8c48ee10d2f2c7d78cca372576cf6a2dacf9ee3f
Paulo Cesar Ferreira <paulocesar@dzigual.com.br>
2017-02-28 11:04:11 -0300

    Criado um exemplo sobre classes e explicado que cada objeto criado com 'new' é diferente um do outro

---
+++ revisao_sobre_classes.rb
@@ -0,0 +1,16 @@
+class Person
+	# attributos da classe
+	# - age
+	# - name
+	# - eyeColors
+
+	# métodos da classe
+	# - walk
+	# - say
+	# - think
+
+end
+
+# Dois objetos, diferentes
+@person1 = Person.new
+@person2 = Person.new
